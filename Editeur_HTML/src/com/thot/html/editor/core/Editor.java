52f09ce02dd029f131d3f18fc45235ec80a07a0c
Didier <sepecat@wanadoo.fr>
2017-08-15 20:30:44 +0200

    Editeur HTML.

--- Editeur_HTML/src/com/thot/html/editor/core/Editor.java
+++ Editeur_HTML/src/com/thot/html/editor/core/Editor.java
@@ -1,22 +1,32 @@
 package com.thot.html.editor.core;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Accordion;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
+import javafx.scene.web.HTMLEditor;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
@@ -24,7 +34,42 @@ import javafx.stage.WindowEvent;
 public final class Editor extends Application
 {
 
-	private static final double RATIO = 0.9;
+	private static final double RATIO              = 0.9;
+	private static final double HORIZONTAL_PADDING = 10;
+	private static final double VERTICAL_PADDING   = 8;
+	private static final double HORIZONTAL_SPACING = 10;
+	private static final double VERTICAL_SPACING   = 8;
+
+	private static final Insets GRID_PADDING = new Insets(VERTICAL_PADDING,HORIZONTAL_PADDING,VERTICAL_PADDING,HORIZONTAL_PADDING);
+
+	private static enum WIDGET_ID
+	{
+
+		MENUBAR,
+		ACCORDION,
+		CENTER_AREA,
+		STATUS_BAR,
+		PREFERENCES_PANE,
+		METADATA_PANE,
+		MAIL_PANE;
+
+	}
+
+	public static enum FIELD_ID
+	{
+
+		SEND_TO,
+		SUBJECT;
+
+	}
+
+	public static enum MENUITEM_ID
+	{
+
+		ABOUT,
+		EXIT;
+
+	}
 
 	private Stage stage = null;
 	private Scene scene = null;
@@ -49,6 +94,19 @@ public final class Editor extends Application
 
 		final StackPane stackPane = new StackPane();
 
+		stackPane.setUserData(WIDGET_ID.CENTER_AREA);
+
+		final ObservableList<Node> children = stackPane.getChildren();
+
+		if (children != null)
+		{
+
+			final HTMLEditor htmlEditor = new HTMLEditor();
+
+		    children.add(htmlEditor);
+
+		}
+
 		return stackPane;
 
 	}
@@ -59,6 +117,7 @@ public final class Editor extends Application
 		final HBox statusBar = new HBox();
 
 		statusBar.setPrefHeight(30);
+		statusBar.setUserData(WIDGET_ID.STATUS_BAR);
 
 		return statusBar;
 
@@ -69,8 +128,9 @@ public final class Editor extends Application
 
 		final TitledPane titledPane = new TitledPane();
 
-		titledPane.setText       ("Préférences");
-		titledPane.setPrefHeight (400          );
+		titledPane.setText       ("Préférences"             );
+		titledPane.setPrefHeight (400                       );
+		titledPane.setUserData   (WIDGET_ID.PREFERENCES_PANE);
 
 		final Pane gridPane = new GridPane();
 
@@ -87,12 +147,105 @@ public final class Editor extends Application
 
 	}
 
+	private final TitledPane createMetadataPane()
+	{
+
+		final TitledPane titledPane = new TitledPane();
+
+		titledPane.setText       ("Métadonnées"          );
+		titledPane.setPrefHeight (400                    );
+		titledPane.setUserData   (WIDGET_ID.METADATA_PANE);
+
+		final Pane gridPane = new GridPane();
+
+		final ObservableList<Node> children = gridPane.getChildren();
+
+		if (children != null)
+		{
+
+		}
+
+		titledPane.setContent(gridPane);
+
+		return titledPane;
+
+	}
+
+	private final TitledPane createMailPane()
+	{
+
+		final TitledPane titledPane = new TitledPane();
+
+		titledPane.setText       ("Messagerie"       );
+		titledPane.setPrefHeight (200                );
+		titledPane.setUserData   (WIDGET_ID.MAIL_PANE);
+
+		final GridPane gridPane = new GridPane();
+
+		final ObservableList<Node> children = gridPane.getChildren();
+
+		if (children != null)
+		{
+
+			gridPane.setPadding (GRID_PADDING      );
+			gridPane.setHgap    (HORIZONTAL_SPACING);
+			gridPane.setVgap    (VERTICAL_SPACING  );
+
+			final ColumnConstraints leftConstraint = new ColumnConstraints();
+
+			leftConstraint.setMinWidth(100);
+			leftConstraint.setHalignment(HPos.RIGHT);
+
+			final ColumnConstraints rightConstraint = new ColumnConstraints();
+
+			rightConstraint.setHgrow(Priority.NEVER);
+
+			gridPane.getColumnConstraints().addAll(leftConstraint,rightConstraint);
+
+			final ObservableList<String> sendItems = FXCollections.observableArrayList("To:", "Cc:", "Bcc:");
+
+	        final ChoiceBox<String> sendTo = new ChoiceBox<>(sendItems);
+
+	        sendTo.setPrefWidth(150);
+
+	        final TextField sendToField = new TextField();
+
+	        sendToField.setPrefWidth (400             );
+	        sendToField.setUserData  (FIELD_ID.SEND_TO);
+
+	        gridPane.addRow(0,sendTo,sendToField);
+
+	        final Label subjectLabel = new Label("Sujet :");
+
+	        subjectLabel.setAlignment (Pos.CENTER_RIGHT        );
+	        subjectLabel.setMaxWidth  (Double.POSITIVE_INFINITY);
+
+	        GridPane.setConstraints(subjectLabel,0,1);
+
+	        final TextField subjectField = new TextField();
+
+	        subjectField.setPrefWidth (400             );
+	        subjectField.setUserData  (FIELD_ID.SUBJECT);
+
+	        GridPane.setConstraints(subjectField,1,1);
+
+	        gridPane.addRow(1,subjectLabel,subjectField);
+
+		}
+
+		titledPane.setContent(gridPane);
+
+		return titledPane;
+
+	}
+
 	private final Accordion createAccordion()
 	{
 
 		final Accordion accordion = new Accordion();
 
-		accordion.setPrefWidth(400);
+		accordion.setPrefWidth (450                );
+		accordion.setUserData  (WIDGET_ID.ACCORDION);
 
 		final ObservableList<TitledPane> children = accordion.getPanes();
 
@@ -100,6 +253,8 @@ public final class Editor extends Application
 		{
 
 			children.add(this.createPreferencesPane());
+			children.add(this.createMetadataPane()   );
+			children.add(this.createMailPane()       );
 
 		}
 
@@ -112,7 +267,8 @@ public final class Editor extends Application
 
 		final MenuItem menuItem = new MenuItem();
 
-		menuItem.setText("Quitter");
+		menuItem.setText     ("Quitter"       );
+		menuItem.setUserData (MENUITEM_ID.EXIT);
 
 		menuItem.setOnAction(new EventHandler<ActionEvent>()
 		{
@@ -172,7 +328,8 @@ public final class Editor extends Application
 
 		final MenuItem menuItem = new MenuItem();
 
-		menuItem.setText("\u00C0 propos\u2026");
+		menuItem.setText     ("\u00C0 propos\u2026");
+		menuItem.setUserData (MENUITEM_ID.ABOUT    );
 
 		menuItem.setOnAction(new EventHandler<ActionEvent>()
 		{
@@ -223,6 +380,8 @@ public final class Editor extends Application
 
 		final MenuBar menuBar = new MenuBar();
 
+		menuBar.setUserData(WIDGET_ID.MENUBAR);
+
 		final ObservableList<Menu> menus = menuBar.getMenus();
 
 		if (menus != null)
@@ -242,7 +401,8 @@ public final class Editor extends Application
 
 		final BorderPane borderPane = new BorderPane();
 
-		borderPane.setCache(true);
+		borderPane.setCache   (true         );
+		borderPane.setPadding (new Insets(0));
 
 		borderPane.setTop    (this.createMenuBar()  );
 		borderPane.setLeft   (this.createAccordion());
